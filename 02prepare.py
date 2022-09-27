1d48b550cbb09ac97096ed31eddc03a3e7194668
CodingQueen1001 <ashleyhillstead13@gmail.com>
2022-09-27 13:39:59 -0600

    September 27

---
+++ 02prepare.py
@@ -0,0 +1,16 @@
+'''
+02 Prepare: Store and Display Information with Better Formatting
+Ashley Hillstead
+September 20, 2022
+'''
+words = "the cat IN THE hat"
+print(words.capitalize())
+print(words.title())
+print(words.upper())
+print(words.lower())
+print(words.count("t"))
+print(words.lower().count("t")) 
+#This resulted in a 4, because everything was lowercase so it counted the capital T while the one before didn't
+
+
+
