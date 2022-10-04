b762010617205bdc1ab6f60ba95e4c4d543f84c4
CodingQueen1001 <ashleyhillstead13@gmail.com>
2022-10-04 14:33:28 -0600

    October 4th (2)

---
+++ 03prepare.py
@@ -0,0 +1,85 @@
+'''
+03 Prepare: Preparation Material
+Ashley Hillstead
+October 4, 2022
+'''
+# #II. Using different data types
+# number = 7
+# print(f"The number is {number}.")
+
+# #III. Operators and Different Data Types
+# color = 'blue'
+# animal = 'horse'
+# print(color + animal)
+# print(color + ' ' + animal + '!')
+# combined_words = color + ' ' + animal + ' '
+# print(combined_words)
+
+# #III part II
+# boys_count = 10
+# girls_count = 12
+# print(boys_count + girls_count)
+# total_count = boys_count + girls_count
+# print(total_count)
+
+# #IV. Mixing strings and numbers
+# apple_count = 5
+# print("You have " + apple_count + " apples") #ERROR!
+
+# #IV part II
+# length_num = 50
+# width_num = 10
+# print(length_num + width_num)
+# length_string = str(length_num)
+# width_string = str(width_num)
+# print(length_string + width_string)
+
+# #IV part III
+# quantity_from_user = input("How many items do you have? ")
+# quantity_number = int(quantity_from_user)
+# double_number = quantity_number * 2
+
+#     #WRONG:
+# print("Twice as many is: " + double_number)
+#     #Right:
+# double_string = str(double_number)
+# print("Twice as many is: " + double_string)
+# print("Twice as many is " + str(double_number))
+
+# #IV part IV
+#     #Using two lines
+# people_string = input("How many people are in the room? ")
+# people_number = int(people_string)
+
+#     #Using one line
+# people_number = int(input("How many people are in the room? "))
+
+#     #The same works for folating point numbers:
+# length_number = float(input("What is the length? "))
+
+# #V. Expressions and Mathmatical Operations
+#     #Add is +
+# print(3 + 4)
+#     #Subtract is -
+# print(3 - 4)
+#     #Multiply is *
+# print(3 * 4)
+#     #Divide is /
+# print(15 / 4)
+#     #Divide and drop remainder is //
+# print(15 // 4)
+#     #Remainder or Modulus is %
+# print(25 % 7)
+#     #Exponent(to the power of) is **
+# print(3 ** 4)
+
+
+
+
+
+
+
+
+
+
+
