149d936da46537c4dc4593c7388cb3d028cd3530
CodingQueen1001 <ashleyhillstead13@gmail.com>
2022-10-04 15:24:21 -0600

    October 4th (2)

---
+++ 03check.py
@@ -0,0 +1,12 @@
+age = int(input("How old are you? "))
+birthday = age + 1
+print(f"On your next birthday, you will be {birthday}")
+print()
+cartons = int(input("How many egg cartons do you have? "))
+eggs = cartons * 12
+print(f"You have {eggs} eggs")
+print()
+cookies = int(input("How many cookies do you have? "))
+people = int(input("How many people are there? "))
+per = cookies // people
+print(f"Each person may have {per} cookies")
---
+++ 03teach.py
@@ -0,0 +1,48 @@
+# #basic
+# side_square = float(input("What is the length of a side of the square? "))
+# area_square = side_square ** 2
+# print(f"The area of the square is: {area_square}")
+# length_rectangle = float(input("What is the length of the rectangle? "))
+# width_rectangle = float(input("What is the width of the rectangle? "))
+# area_rectangle = length_rectangle * width_rectangle
+# print(f"The area of the rectangle is: {area_rectangle}")
+# radius_circle = float(input("What is the radius of the cirlce? "))
+# area_circle = 3.14 * (radius_circle ** 2)
+# print(f"The area of the circle is: {area_circle}")
+
+# #stretch 1
+# import math
+# radius_circle = float(input("What is the radius of the cirlce? "))
+# area_circle = math.pi * (radius_circle ** 2)
+# print(f"The area of the circle is: {area_circle}")
+# #stretch 2
+# input = float(input("What is the value to be used? "))
+# area_square = input ** 2
+# print(f"The area of the square is: {area_square}")
+# area_rectangle = input * input
+# print(f"The area of the rectangle is: {area_rectangle}")
+# volume_cube = input ** 3
+# print(f"Volume of a cube: {volume_cube}")
+# volume_sphere = (4 / 3) * math.pi * (input ** 3)
+# print(f"Volume of a sphere: {volume_sphere}")
+# #stretch 3
+# side = float(input("What is the length of a side of the square (in cm)? "))
+# area = side ** 2
+# print(f"The area of the square is: {area} cm^2")
+# print(f"The area of the square is: {area / 10000} m^2")
+
+# length = float(input("What is the length of rectangle (in cm)? "))
+# width = float(input("What is the width of the rectangle (in cm)? "))
+# area = length * width
+# print(f"The area of the rectangle is: {area} cm^2")
+# print(f"The area of the rectangle is: {area / 10000} m^2")
+
+# radius = float(input("What is the radius of the circle (in cm)? "))
+# area = 3.14 * (radius ** 2)
+# print(f"The area of the circle is: {area} cm^2")
+# print(f"The area of the circle is {area / 10000} m^2")
+
+
+
+
+
